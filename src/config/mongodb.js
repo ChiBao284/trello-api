40ae53fe26d3c326175d7af3fdd0efb7ca32d432
Chibao284 <chibao284@gmail.com>
2023-11-25 17:58:42 +0700

    connect mongoDB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,37 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { env } from './environment';
+
+// Khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
+let trelloDatabaseInstance = null;
+
+// Khởi tạo đối tưởng mongodbClientInstance để connect tới MongoDB
+const mongodbClientInstance = new MongoClient(env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+// Kết nối Database
+export const CONNECT_DB = async () => {
+    // Gọi kết nói tới MongoDB Atlas với URI đã khai báo
+
+    await mongodbClientInstance.connect();
+
+    trelloDatabaseInstance = mongodbClientInstance.db(
+        process.env.DATABASE_NAME,
+    );
+};
+
+export const CLOSE_DB = async () => {
+    // Đóng kết nối tới MongoDB
+    await mongodbClientInstance.close();
+};
+
+export const GET_DB = () => {
+    if (!trelloDatabaseInstance) {
+        throw new Error('Must connect to Database first!');
+    }
+    return trelloDatabaseInstance;
+};
--- src/server.js
+++ src/server.js
@@ -1,38 +1,40 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
 
 import express from 'express';
-import { mapOrder } from '~/utils/sorts.js';
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
+import exitHook from 'async-exit-hook';
+import { env } from '~/config/environment';
 
-const app = express();
+const START_SERVER = () => {
+    const app = express();
+    const hostname = env.APP_HOST;
+    const port = env.APP_PORT;
 
-const hostname = 'localhost';
-const port = 8017;
+    app.get('/', (req, res) => {
+        res.end('<h1>Hello World!</h1><hr>');
+    });
 
-app.get('/', (req, res) => {
-    // Test Absolute import mapOrder
-    console.log(
-        mapOrder(
-            [
-                { id: 'id-1', name: 'One' },
-                { id: 'id-2', name: 'Two' },
-                { id: 'id-3', name: 'Three' },
-                { id: 'id-4', name: 'Four' },
-                { id: 'id-5', name: 'Five' },
-            ],
-            ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-            'id',
-        ),
-    );
-    res.end('<h1>Hello World!</h1><hr>');
-});
+    app.listen(port, hostname, () => {
+        // eslint-disable-next-line no-console
+        console.log(
+            `Hello ${env.AUTHOR}, I am running at http://${hostname}:${port}/`,
+        );
+    });
+    // Close connect mongoDB
+    exitHook(() => {
+        CLOSE_DB();
+        console.log('Exit-Hook: Quit app');
+    });
+};
 
-app.listen(port, hostname, () => {
-    // eslint-disable-next-line no-console
-    console.log(
-        `Hello Trung Quan Dev, I am running at http://${hostname}:${port}/`,
-    );
-});
+// Chạy kết nối databse start server
+
+(async () => {
+    try {
+        await CONNECT_DB();
+        START_SERVER();
+    } catch (error) {
+        console.log(error);
+        process.exit();
+    }
+})();
